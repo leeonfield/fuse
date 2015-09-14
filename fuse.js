72def8985997b27e7eb491d38e637f7696e78c7b
lijundong <1752220874@qq.com>
2015-09-14 17:45:57 +0800

    添加了背景经纬线，视觉上的地图效果很大的提升了

--- fuse.js
+++ fuse.js
@@ -1,6 +1,7 @@
 var mapLength = 2000;
 
-
+var lineCoordinate_x = [60, 120, 180, 240, 300, 360, 420, 480];
+var lineCoordinate_y = [60, 120, 180, 240, 300, 360, 420, 480];
 
 var list = new Array();
 var colorList = ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#00FFFF", "#0000FF", "#8B00FF"];
@@ -62,9 +63,10 @@ function getCoordinates(e) {
 	player.target_y = y;
 }
 
+function getMoveDis() {
 
-function updata() {
-
+	var moveDis_x = 0;
+	var moveDis_y = 0;
 	var dis_x = player.target_x - 300;
 	var dis_y = player.target_y - 300;
 	var distance = Math.sqrt(Math.pow(dis_x, 2) + Math.pow(dis_y, 2));
@@ -74,52 +76,56 @@ function updata() {
 		var mov_y = player.speed * (dis_y / distance);
 
 		if ((player.x + mov_x) > mapLength) {
-			player.x = mapLength - player.radius;
+			moveDis_x = mapLength - player.radius - player.x;
 			player.target_x = 300;
 		} else if ((player.x + mov_x) < 0) {
-			player.x = player.radius;
+			moveDis_x = player.radius - player.x;
 			player.target_x = 300;
 		} else {
-			player.x += mov_x;
+			moveDis_x = mov_x;
 		}
 		if ((player.y + mov_y) > mapLength) {
-			player.y = mapLength - player.radius;
+			moveDis_y = mapLength - player.radius - player.y;
 			player.target_y = 300;
 		} else if ((player.y + mov_y) < 0) {
-			player.y = player.radius;
+			moveDis_y = player.radius - player.y;
 			player.target_y = 300;
 		} else {
-			player.y += mov_y;
+			moveDis_y = mov_y;
 
 		}
-
-
 	}
+	return {
+		moveDis_x, moveDis_y
+	};
+}
 
-
+function updata() {
+	player.x += getMoveDis().moveDis_x;
+	player.y += getMoveDis().moveDis_y;
 	for (var i = 300 - list.length; i >= 0; i--) {
-
 		var x = new ListNode(colorList[randomColor()], randomRadius(), randomCoordinate(), randomCoordinate());
 		list.push(x);
 	}
-
 }
 
 function draw(context) {
 	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+	//绘制背景线条
+	for (var i = 0; i < 8; i++) {
 
-	for (var i = player.speed; i < 500; i += 60) {
-		var x = i +
+		lineCoordinate_x[i] = (lineCoordinate_x[i] + 500 - getMoveDis().moveDis_x) % 500;
+		lineCoordinate_y[i] = (lineCoordinate_y[i] + 500 - getMoveDis().moveDis_y) % 500;
 
-			context.beginPath();
-		context.lineTo(i, 0);
-		context.lineTo(i, 500);
+		context.beginPath();
+		context.lineTo(lineCoordinate_x[i], 0);
+		context.lineTo(lineCoordinate_x[i], 500);
 		context.strokeStyle = "gray";
 		context.stroke();
 
 		context.beginPath();
-		context.lineTo(0, i);
-		context.lineTo(500, i);
+		context.lineTo(0, lineCoordinate_y[i]);
+		context.lineTo(500, lineCoordinate_y[i]);
 		context.strokeStyle = "gray";
 		context.stroke();
 	}
@@ -127,7 +133,6 @@ function draw(context) {
 
 
 	for (var i = 0; i < list.length; i++) {
-
 
 		//判断接触
 		if (getDistance(player.x, player.y, list[i].x, list[i].y) <= player.radius) {
@@ -148,16 +153,12 @@ function draw(context) {
 		}
 	}
 
-
-
 	//把用户的点绘制在（300,300）
 	context.beginPath();
 	context.fillStyle = player.color;
 	context.arc(300, 300, player.radius, 0, Math.PI * 2);
 	context.fill();
-
 	document.getElementById("status").innerHTML = " Score:" + player.score + "</br>" + " Range:.....";
-
 }
 
 var i = 0;
@@ -170,7 +171,7 @@ function func() {
 
 	updata();
 	draw(cxt);
-	console.log(player.x + "        :    " + player.y);
+	//console.log(player.x + "        :    " + player.y);
 	requestAnimationFrame(func);
 
 }
